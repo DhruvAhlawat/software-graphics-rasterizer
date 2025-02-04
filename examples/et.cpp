698a5febcdfc2a0fb812e2e5e9cdfe7c817b3f42
Aditya Upadhyay <me1210275@iitd.ac.in>
2025-02-04 22:43:59 +0530

    Clock modifications

--- examples/et.cpp
+++ examples/et.cpp
@@ -2,7 +2,7 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <ctime>
 
-namespace R = COL781::Software;
+namespace R = COL781::Hardware;
 using namespace glm;
 
 int main() {
@@ -13,7 +13,7 @@ int main() {
 
     R::ShaderProgram program = r.createShaderProgram(
         r.vsColorTransform(),
-        r.fsIdentity()
+        r.fsConstant()
     );
 
     float vertices[] = {
@@ -23,18 +23,13 @@ int main() {
          0.5,  0.5, 0.0, 1.0
     };
 
-    float colors[] = {
-        1.0, 1.0, 1.0, 1.0,
-        1.0, 1.0, 1.0, 1.0,
-        1.0, 1.0, 1.0, 1.0,
-        1.0, 1.0, 1.0, 1.0
+    int triangles[] = {
+        0, 1, 2,
+        1, 2, 3
     };
-
-    int triangles[] = {0, 1, 2, 1, 2, 3};
 
     R::Object square = r.createObject();
     r.setVertexAttribs(square, 0, 4, 4, vertices);
-    r.setVertexAttribs(square, 1, 4, 4, colors);
     r.setTriangleIndices(square, 2, triangles);
 
     mat4 view = translate(mat4(1.0f), vec3(0.0f, 0.0f, -2.0f)); 
@@ -48,30 +43,44 @@ int main() {
         float minuteAngle = -radians(6.0f * ltm->tm_min);
         float secondAngle = -radians(6.0f * ltm->tm_sec);
 
-        r.clear(vec4(0.0, 0.0, 0.0, 1.0));
+        r.clear(vec4(1.0, 1.0, 1.0, 1.0));
         r.useShaderProgram(program);
 
-        // Draw hour hand
+        // hour hand
         mat4 model = rotate(mat4(1.0f), hourAngle, vec3(0.0f, 0.0f, 1.0f)) * translate(mat4(1.0f), vec3(0.0f, 0.15f, 0.0f)) * scale(mat4(1.0f), vec3(0.08f, 0.3f, 1.0f));
         r.setUniform(program, "transform", projection * view * model);
         r.drawObject(square);
 
-        // Draw minute hand
+        // minute hand
         model = rotate(mat4(1.0f), minuteAngle, vec3(0.0f, 0.0f, 1.0f)) * translate(mat4(1.0f), vec3(0.0f, 0.2f, 0.0f)) * scale(mat4(1.0f), vec3(0.05f, 0.4f, 1.0f));
         r.setUniform(program, "transform", projection * view * model);
         r.drawObject(square);
 
-        // Draw second hand
+        // second hand
         model = rotate(mat4(1.0f), secondAngle, vec3(0.0f, 0.0f, 1.0f)) * translate(mat4(1.0f), vec3(0.0f, 0.25f, 0.0f)) * scale(mat4(1.0f), vec3(0.03f, 0.6f, 1.0f));
         r.setUniform(program, "transform", projection * view * model);
         r.drawObject(square);
 
-        // Draw hour markings
+        // hour markings, can also make them cooler ig
         for (int i = 0; i < 12; ++i) {
             float angle = radians(30.0f * i);
-            model = translate(mat4(1.0f), vec3(0.8f * sin(angle), 0.8f * cos(angle), 0.0f)) * scale(mat4(1.0f), vec3(0.02f, 0.1f, 1.0f));
+            model = rotate(mat4(1.0f), angle, vec3(0.0f, 0.0f, 1.0f))
+                    * translate(mat4(1.0f), vec3(0.0f, 0.75f, 0.0f))
+                    * scale(mat4(1.0f), vec3(0.02f, 0.09f, 1.0f));
+
             r.setUniform(program, "transform", projection * view * model);
+            r.setUniform<vec4>(program, "color", vec4(1.0, 0.0, 0.0, 1.0));
             r.drawObject(square);
+
+            for (int j=0; j<5; j++){    // minute markings
+                float s_angle = radians(30.0f*i+6*j);
+                model = rotate(mat4(1.0f), s_angle, vec3(0.0f, 0.0f, 1.0f))
+                    * translate(mat4(1.0f), vec3(0.0f, 0.75f, 0.0f))
+                    * scale(mat4(1.0f), vec3(0.02f, 0.03f, 1.0f));
+
+                r.setUniform(program, "transform", projection * view * model);
+                r.drawObject(square);
+            }
         }
 
         r.show();
