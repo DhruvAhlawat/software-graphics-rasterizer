237012158972df5417a884b44c7202e0bc5c0652
Aditya Upadhyay <me1210275@iitd.ac.in>
2025-02-07 19:07:54 +0530

    currently not working blinn phong

--- src/sw.cpp
+++ src/sw.cpp
@@ -102,12 +102,68 @@ namespace COL781 {
 				glm::vec3 light_dir = uniforms.get<glm::vec3>("lightDir");
 				glm::vec3 ambient_col = uniforms.get<glm::vec3>("ambientColor");
 				glm::vec3 obj_col = uniforms.get<glm::vec3>("objectColor");
+
 				glm::vec3 normal = in.get<glm::vec3>(1);
 				normal = normalize(normal);
-				float intensity = std::max(dot(normal, light_dir), 0.0f);
-				glm::vec3 diffuse = intensity * light_col * obj_col;
-				glm::vec3 color = (diffuse + ambient_col);
-				// glm::vec3 color = diffuse;
+
+				// float intensity = std::max(dot(normal, light_dir), 0.0f);
+				// glm::vec3 diffuse = intensity * light_col * obj_col;
+				// glm::vec3 color = (diffuse + ambient_col);
+				
+				glm::vec3 I = pow(light_col, glm::vec3(2.2f));
+				glm::vec3 ka = pow(ambient_col, glm::vec3(2.2f));
+				glm::vec3 kd = pow(obj_col, glm::vec3(2.2f));
+
+				float diff = std::max(dot(normalize(normal), normalize(light_dir)), 0.0f);
+
+				glm::vec3 diffuse = diff * I * kd;
+				glm::vec3 linColor = ka + diffuse;
+				glm::vec3 color = pow(linColor, glm::vec3(1.0f / 2.2f));
+
+				return glm::vec4(color, 1.0f);
+			};
+		}
+
+		FragmentShader Rasterizer::fsSpecularLighting()
+		{
+			return [](const Uniforms &uniforms, const Attribs &in) 
+			{
+				glm::vec3 light_col = uniforms.get<glm::vec3>("lightColor");
+				glm::vec3 light_dir = uniforms.get<glm::vec3>("lightDir");
+				glm::vec3 ambient_col = uniforms.get<glm::vec3>("ambientColor");
+				glm::vec3 obj_col = uniforms.get<glm::vec3>("objectColor");
+
+				glm::vec3 normal = in.get<glm::vec3>(1);
+				normal = normalize(normal);
+
+
+				glm::vec3 I = pow(light_col, glm::vec3(2.2f));
+				glm::vec3 ka = pow(ambient_col, glm::vec3(2.2f));
+				glm::vec3 kd = pow(obj_col, glm::vec3(2.2f));
+
+				float diff = std::max(dot(normalize(normal), normalize(light_dir)), 0.0f);
+
+				glm::vec3 diffuse = diff * I * kd;
+				glm::vec3 linColor = ka + diffuse;
+				glm::vec3 color = pow(linColor, glm::vec3(1.0f / 2.2f));
+
+				// Blinn-Phong Specular lighting:
+
+				glm::vec3 viewPos = uniforms.get<glm::vec3>("viewPos");
+				glm::vec4 worldpos = in.get<glm::vec4>(2);
+				glm::vec3 view_dir = normalize(glm::vec3(viewPos) - glm::vec3(worldpos));
+				glm::vec3 l = normalize(light_dir);
+				glm::vec3 h = normalize(view_dir + l);
+
+				float s_diff = std::max(dot(normal, l), 0.0f);
+				int n=10;
+				float s_spec = pow(std::max(dot(normal, h), 0.0f), n);
+
+				glm::vec3 specular = s_spec * I;
+				glm::vec3 color_spec = specular * kd;
+				color = color + color_spec;
+				
+
 				return glm::vec4(color, 1.0f);
 			};
 		}
